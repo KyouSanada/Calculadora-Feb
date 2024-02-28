ccc065cc06da96da84f56738a6b6b5c292af93f3
KyouSanada <152229662+KyouSanada@users.noreply.github.com>
2024-02-27 22:00:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const error = document.getElementById("error");
 const FLU = document.getElementById("flu");
 const MAN = document.getElementById("man");
 
-function hidratacion (peso){    
+
+function hidratacion (peso){   
     let flujo;
     superficieCorporal = (((peso)*4 + 7) / ((peso) + 90));
     console.log(superficieCorporal);
@@ -19,24 +20,42 @@ function hidratacion (peso){
         }
 
     }
-    else{flujo = Math.round(superficieCorporal*1500)};
-    return flujo
+    else{flujo = superficieCorporal;
+    };
+    return flujo 
 };
 
-boton.addEventListener("click", () =>{
+boton.addEventListener("click", () =>{ 
     const peso = document.getElementById("peso").valueAsNumber;
     let flujo;
-    if (peso > 0){
+    if (peso > 0 && peso <= 30){
         error.style.display = "none";
         FLU.style.display = "block"
         MAN.style.display = "block";
         flujo = hidratacion(peso);
-        FLU.innerHTML = flujo + "cc/dia" + " - " + Math.round(flujo/24) + "cc/hr";
-        MAN.innerHTML = "m+1/2m: " + flujo*1.5 + "cc/dia"+ " - " + Math.round(flujo*1.5 /24) + "cc/hr";
+        FLU.innerHTML = 
+        flujo  + "cc/dia" + " - " + Math.round(flujo/24)  + "cc/hr";
+        MAN.innerHTML = 
+        "m+1/2m: " + flujo*1.5  + "cc/dia"+ " - " + Math.round(flujo*1.5 /24) + "cc/hr";
     }
+        else if(peso >30){
+            error.style.display = "none";
+            FLU.style.display = "block";
+            MAN.style.display = "block";
+            flujo = hidratacion(peso);
+            FLU.innerHTML = 
+                Math.round(flujo * 1500)  + "cc/dia o " 
+                + Math.round(flujo*2000)  + "cc/dia";
+            MAN.innerHTML = 
+                "m+1/2m: " + Math.round(flujo*1500*1.5)  + "cc/dia o " 
+                + Math.round(flujo*2000*1.5) + "cc/dia";
+        }
     else{error.style.display = "block";
             FLU.style.display = "none";
             MAN.style.display = "none"}
 });
+
+
+
 
 
